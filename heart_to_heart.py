17a8810ceb29315338703da90e294cd3c0f2ab9c
Aima <aimashahid155@gmail.com>
2026-07-13 23:01:14 +0200

    Create heart_to_heart.py

---
+++ heart_to_heart.py
@@ -0,0 +1,7 @@
+"sky_is_gonna_orange"
+weather=("rainy")
+     time=["evening"]
+color=[orange]
+sitution=("standing in balcony and enjoying this weather")
+if "pink" in color:
+    print("exact  color of sky")
